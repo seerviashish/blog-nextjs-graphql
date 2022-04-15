a86ed5ae00021fab63956acbc999807b157a9647
Ashish Chaudhary <seerviashish17@gmail.com>
2022-04-15 11:55:41 +0530

    feat: client init

---
+++ client/next.config.js
@@ -0,0 +1,31 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  i18n: {
+    // These are all the locales you want to support in
+    // your application
+    locales: ['en-US', 'hi-IN', 'gu-IN'],
+    // This is the default locale you want to be used when visiting
+    // a non-locale prefixed path e.g. `/hello`
+    defaultLocale: 'en-US',
+    // This is a list of locale domains and the default locale they
+    // should handle (these are only required when setting up domain routing)
+    // Note: subdomains must be included in the domain value to be matched e.g. "fr.example.com".
+    // domains: [
+    //   {
+    //     domain: 'example.com',
+    //     defaultLocale: 'en-US',
+    //   },
+    //   {
+    //     domain: 'hi.example.com',
+    //     defaultLocale: 'hi-IN',
+    //   },
+    //   {
+    //     domain: 'gu.example.com',
+    //     defaultLocale: 'gu-IN',
+    //   }
+    // ],
+  },
+}
+
+module.exports = nextConfig
